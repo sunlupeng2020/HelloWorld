e625b2b54defa847e5e8bb232ca5f8ffff22d7cf
sunlupeng2020 <61284291+sunlupeng2020@users.noreply.github.com>
2020-02-23 09:37:03 +0800

    提交了最初的helloworld程序。
    提交了最初的helloworld程序。

---
+++ lx001.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo "这是标题"; ?></title>
+	<style>
+	    body{background-color: <?php echo 'black'; ?>}
+	</style>
+	<script>
+		alert(<?php echo 10 + 20; ?>);
+	</script>
+
+</head>
+<body>
+	<h2>PHP和HTML代码的互相嵌套</h2>
+	<font color="<?php echo'white'; ?>">
+		<?php echo '<strong>黑马</strong>';?>程序员
+	</font>
+</body>
+</html>
+
+
